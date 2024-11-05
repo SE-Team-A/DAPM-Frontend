782eddc6a7d78920b5fabc0d5e2392c3be951103
hseindirani <dirani.hussein93@gmail.com>
2024-11-05 09:50:20 +0100

    deletepopup

--- src/components/adminDashboard/deletePopup.tsx
+++ src/components/adminDashboard/deletePopup.tsx
@@ -1,14 +1,14 @@
 /**
  * Author:
- * - Mahdi El Dirani
+ * - Hussein Dirani
  * 
  * Description:
- * Add Member Dialog
+ * Delete Member Dialog
  */
 import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import CloseIcon from '@mui/icons-material/Close';
 import { useAuth } from '../../auth/authProvider';
-
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
 interface DeleteMemberPopupProps {
     openDeleteMemberPopup: boolean;
@@ -27,14 +27,17 @@ const DeleteMemberPopup: React.FC<DeleteMemberPopupProps> = ({ openDeleteMemberP
 
             <div className="fixed inset-0 flex w-screen items-center justify-center sm:p-4 backdrop-blur-md 
             ">
-                <DialogPanel className="relative max-w-lg space-y-4 border  bg-[#121212] px-12 py-8 rounded sm:border-solid border-white border-none">
+                <DialogPanel className="relative max-w-lg space-y-4 border  bg-[#121212] px-20 py-6 rounded sm:border-solid border-white border-none">
                     {/* <CloseIcon onClick={() => setOpenDeleteMemberPopup(false)} className="cursor-pointer absolute text-white right-5 top-5"></CloseIcon> */}
+                    <div className='flex justify-center w-full'>
 
-                    <DialogTitle className="font-bold text-white sm:text-3xl text-xl text-center ">Delete Member</DialogTitle>
-                    <h3 className='text-white w-full text-center'>Are u sure u want to delete?</h3>
+                    <HighlightOffIcon style={{fontSize:80}} className='text-center text-red-500'></HighlightOffIcon>
+                    </div>
+                    <DialogTitle className="font-bold text-white sm:text-3xl text-xl text-center ">Are you sure?</DialogTitle>
+                    <h3 className='text-[#959595] w-full text-center'>Do you really want to delete this records?This process cannot be undone</h3>
                     <div className="w-full flex justify-center mt-6 h-fit text-white">
-                        <button onClick={() => { setOpenDeleteMemberPopup(false) }} className="text-white text-base sm:w-fit w-full sm:p-1 p-2 px-6  bg-[#8758ff] mr-1">Cancel</button>
-                        <div onClick={handleDelete} className="text-white text-base sm:w-fit w-full sm:p-1 p-2 px-6 bg-red-600 cursor-pointer text-center">Delete</div>
+                        <button onClick={() => { setOpenDeleteMemberPopup(false) }} className="text-white text-base p-2 px-12  bg-[#959595] mr-1">Cancel</button>
+                        <div onClick={handleDelete} className="text-white text-base  p-2 px-12 bg-red-600 cursor-pointer text-center">Delete</div>
                     </div>
 
                     {/* {user?.loadingRegister &&
